8e8cf377793059d509a8b003bff06af9c129c005
Олена Чернишова <elena.chernyshova01@gmail.com>
2026-05-01 10:53:22 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,8 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const leftOffset = (wall.clientWidth - spider.offsetWidth) / 2;
-const topOffset = (wall.clientHeight - spider.offsetHeight) / 2;
+const leftOffset = (wall.offsetWidth - spider.offsetWidth) / 2;
+const topOffset = (wall.offsetHeight - spider.offsetHeight) / 2;
 
 spider.style.left = `${leftOffset}px`;
 spider.style.top = `${topOffset}px`;
