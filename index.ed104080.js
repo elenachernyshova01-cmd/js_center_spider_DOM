1d11b35349efb64b43710ab82777066cbe5c6ff2
Олена Чернишова <elena.chernyshova01@gmail.com>
2026-05-01 12:16:15 +0300

    make build

---
+++ index.ed104080.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=(t.clientWidth-e.offsetWidth)/2,l=(t.clientHeight-e.offsetHeight)/2;e.style.position="absolute",e.style.left="".concat(o,"px"),e.style.top="".concat(l,"px");
+//# sourceMappingURL=index.ed104080.js.map
