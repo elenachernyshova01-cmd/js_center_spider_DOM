1a44db8325004df46a33f333e607e0960b7614d6
Олена Чернишова <elena.chernyshova01@gmail.com>
2026-05-01 12:15:48 +0300

    Center spider with JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,9 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-const leftOffset = (wall.offsetWidth - spider.offsetWidth) / 2;
-const topOffset = (wall.offsetHeight - spider.offsetHeight) / 2;
+const leftOffset = (wall.clientWidth - spider.offsetWidth) / 2;
+const topOffset = (wall.clientHeight - spider.offsetHeight) / 2;
 
+spider.style.position = 'absolute';
 spider.style.left = `${leftOffset}px`;
 spider.style.top = `${topOffset}px`;
